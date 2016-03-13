30737ad5610c7d5a5e79c118a85c78288933604f
Jordan Hu <jordanhu@inside-202-240.wireless.utoronto.ca>
2016-03-13 07:37:09 -0400

    database

---
+++ import_csv.py
@@ -0,0 +1,48 @@
+import csv
+import sys
+import models
+
+def inputLocforAnimal(unique_animal, loc_point, row):
+    if unique_animal.tag_id != row[11]:
+        new_unique_animal = Animal(animal_name="white-fronted-goose", tag_id=row[11])
+        new_loc_point = new_unique_animal_set.create(timestamp=row[2],
+            location_long=int(row[3]), location_lat=int(row[4]),
+            migration_stage=row[7], miration_status=row[8])
+        return new_unqiue_animal
+        new_loc_point
+    else:
+
+
+
+f = open(sys.argv[1], 'rt')
+try:
+    reader = csv.reader(f)
+    for count, row in enumerate(reader):
+        if (count == 0):
+            row = row
+        else:
+            inputLocforAnimal()
+finally:
+    f.close()
+"""
+def inputLocforAnimal(unique_animal, loc_point):
+    loc_point.animal = unqi
+
+f = open('*.csv', 'r')
+unique_animal = Animal(animal_name=)
+for count, line in enumerate(f):
+    if (count === 0):
+        line = line.split(',')
+    else:
+        line = line.split(',')
+        if (line[13] != unique_animal.tag_id):
+            new_unique_animal = Animal(animal_name=line[13], tag_id=line[11])
+            unique_animal = new_unique_animal
+        loc_point = LocationPoint(animal=unique_animal)
+        loc_point.timestamp = line[2]
+        loc_point.location_long = int(line[3])
+        loc_point.location_lat = int(lint[4])
+        loc_point.migration_status = ([line[7])"""
+
+
+    
--- trackr/map/models.py
+++ trackr/map/models.py
@@ -1,6 +1,28 @@
 from django.db import models
-
-class Viewer(models.Models):
-    
+import time
+import datetime
 
 # Create your models here.
+class migration_csv_import(models.Model):
+    event_id = models.IntegerField()
+    timestamp = models.CharField(max_length=50)
+    location_long = models.FloatField()
+    location_lat = models.FloatField()
+    individual_local_identifier = models.IntegerField()
+    study_name = models.CharField(max_length=200)
+
+class Animal(models.Model):
+    animal_name = models.CharField(max_length=200)
+    tag_id = models.IntegerField(default=0)
+    def __str__(self):
+        return self.tag_id
+
+class LocationPoint(models.Model):
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+    timestamp = models.CharField(max_length=50)
+    location_long = models.IntegerField(default=0)
+    location_lat = models.IntegerField(default=0)
+    migration_stage = models.CharField(max_length=50)
+    migration_status = models.CharField(max_length=50)
+    def __str__(self):
+        return location_long, location_lat
